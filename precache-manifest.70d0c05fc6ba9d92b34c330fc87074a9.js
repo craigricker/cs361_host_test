31ffa53d820234d07af88819ae58478c0a5dcf00
craigricker <craigd.ricker@gmail.com>
2018-11-25 20:34:04 -0800

    Updates

---
+++ precache-manifest.70d0c05fc6ba9d92b34c330fc87074a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebdd71a4e6585d519a5d",
+    "url": "/cs361_credible_front/static/js/runtime~main.ebdd71a4.js"
+  },
+  {
+    "revision": "1f99088b40f01e904021",
+    "url": "/cs361_credible_front/static/js/main.1f99088b.chunk.js"
+  },
+  {
+    "revision": "8b0564ae6b390b1ad912",
+    "url": "/cs361_credible_front/static/js/1.8b0564ae.chunk.js"
+  },
+  {
+    "revision": "1f99088b40f01e904021",
+    "url": "/cs361_credible_front/static/css/main.f6c2b94c.chunk.css"
+  },
+  {
+    "revision": "05368b42bba7e1f9578640bc9ea0f6ff",
+    "url": "/cs361_credible_front/index.html"
+  }
+];
